2a8343fd589a0cf795d63e98f1bdc2ae173041c4
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2023-06-15 19:52:45 +0100

    Fixed question 1 using wrong data.

--- 1/script.R
+++ 1/script.R
@@ -9,27 +9,27 @@ library("ggplot2")
 df <- read_xlsx("econ.xlsx")
 
 
-# Filter for years >= 1971
-df <- df[df$tempo >= "1971/01/01", ]
+# Filter for years >= 1976
+df <- df[df$tempo >= "1976/01/01", ]
 
 
-# Create a new data frame with adjusted `ddesemp` and `tpp` columns
+# Create a new data frame with adjusted `ddesemp` and `pop` columns
 ddesemp_mean <- mean(df$ddesemp)
 ddesemp_sd <- sd(df$ddesemp)
 
-tpp_mean <- mean(df$tpp)
-tpp_sd <- sd(df$tpp)
+pop_mean <- mean(df$pop)
+pop_sd <- sd(df$pop)
 
 df_adjusted <- data.frame(
     tempo = df[, c("tempo")],
     ddesemp = lapply(df[, c("ddesemp")], function(ddesemp) (ddesemp - ddesemp_mean) / ddesemp_sd),
-    tpp = lapply(df[, c("tpp")], function(tpp) (tpp - tpp_mean) / tpp_sd)
+    pop = lapply(df[, c("pop")], function(pop) (pop - pop_mean) / pop_sd)
 )
 
 #  Then plot them and save the output
 plot <- ggplot(df_adjusted, aes(x = tempo)) +
     geom_line(aes(y = ddesemp, color = "ddesemp")) +
-    geom_line(aes(y = tpp, color = "tpp")) +
+    geom_line(aes(y = pop, color = "pop")) +
     xlab("Tempo") +
     ylab("") +
     scale_color_manual(values = c("red", "blue")) +
