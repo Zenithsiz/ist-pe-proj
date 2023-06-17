1936aa9f16c55b40af27b926af054c24f5aa105d
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2023-06-17 14:06:38 +0100

    Solved problem 10.

---
+++ 10/script.R
@@ -0,0 +1,31 @@
+#!/bin/env Rscript
+
+# Set the seed for the RNG
+set.seed(1803)
+
+expected_mean <- 48
+significance <- 0.09
+
+# Generate all samples
+true_mean <- 49.2
+true_var <- 4
+n <- 22
+m <- 100
+all_samples <- lapply(1:m, function(idx) {
+    rnorm(n = n, mean = true_mean, sd = sqrt(true_var))
+})
+
+# Then get their t value
+samples_t <- sapply(all_samples, function(samples) {
+    (mean(samples) - expected_mean) / (true_var / sqrt(n))
+})
+
+# And check if they're accepted
+samples_accept <- sapply(samples_t, function(t) {
+    a <- significance / 2
+    t > qnorm(a) && t < qnorm(1 - a)
+})
+
+# Finally average the number of accepts
+accept <- mean(samples_accept)
+message("Accept: ", accept)
