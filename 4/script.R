832451766538b710b65e5950c41dbcbb51e7468e
Filipe Rodrigues <filipejacintorodrigues1@gmail.com>
2023-06-17 15:53:31 +0100

    Fixed 4.

--- 4/script.R
+++ 4/script.R
@@ -9,7 +9,6 @@ expected_mean <- 1.0 / 21.0
 nums <- rexp(nums_len, rate = expected_mean)
 
 # Then generate the cumulative sum of them
-# TODO: Is `max_time` even required?
 nums_cumsum <- cumsum(nums)
 max_time <- ceiling(nums_cumsum[nums_len])
 
